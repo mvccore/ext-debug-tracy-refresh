581136340071159d33639a2712d9c16da0b8edfb
Tom Flídr <tomflidr@gmail.com>
2024-11-21 16:00:59 +0100

    release v5.3

--- src/MvcCore/Ext/Debugs/Tracys/RefreshPanel.php
+++ src/MvcCore/Ext/Debugs/Tracys/RefreshPanel.php
@@ -27,7 +27,7 @@ class RefreshPanel implements \Tracy\IBarPanel {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.0';
+	const VERSION = '5.3.0';
 	
 
 	/**
